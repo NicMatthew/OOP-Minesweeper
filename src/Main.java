b9489bea8895bb307700717c6bb89ae0a9c1097a
GHoran <ghoranmanurung26038@gmail.com>
2023-04-11 15:01:35 +0700

    update test met

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 public class Main implements Runnable {
     GUI gui = new GUI();
+
     public static void main(String[] args) {
+
+        System.out.println("testtttt methewwww");
         new Thread(new Main()).start();
 
     }
 
     @Override
     public void run() {
-        while (true){
+        while (true) {
             gui.repaint();
         }
-    }
+    }// halooo mettt
 }
-
