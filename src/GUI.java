9bea5cb2a4f37306cfa65b9d0ce4ca5fec83c26a
NicMatthew <113076301+NicMatthew@users.noreply.github.com>
2023-04-18 14:27:15 +0700

    stash

--- src/GUI.java
+++ src/GUI.java
@@ -19,14 +19,28 @@ public class GUI extends JFrame {
     int spacing = 5;
     int boxWidth = 80;
     Random random = new Random();
+<<<<<<< Updated upstream
     int count_mines = 16;
+=======
+// <<<<<<< Updated upstream
+    int count_mines = 16;
+// =======
+// >>>>>>> Stashed changes
+>>>>>>> Stashed changes
     int mx = -100;
     int my = -100;
     ArrayList<Integer> cx = new ArrayList<>();
     ArrayList<Integer> cy = new ArrayList<>();
     ArrayList<Mines> mines = new ArrayList<>();
+<<<<<<< Updated upstream
+=======
+// <<<<<<< Updated upstream
+
+// =======
+// >>>>>>> Stashed changes
+>>>>>>> Stashed changes
     public GUI() {
-        while (mines.size() < 16) {
+        while (mines.size() != 16) {
             int ti = random.nextInt(17);
             int tj = random.nextInt(10);
             boolean cek = false;
@@ -34,14 +48,16 @@ public class GUI extends JFrame {
             for (Mines tmines : mines) {
                 if (ti == tmines.getX() && tj == tmines.getY()) {
                     cek = true;
-                    break;
+                    // break;
                 }
             }
             if (cek == true) {
                 continue;
+            }else{
+                Mines temp_mine = new Mines(ti, tj);
+                mines.add(temp_mine);
             }
-            Mines temp_mine = new Mines(ti, tj);
-            mines.add(temp_mine);
+            
         }
         this.setTitle("mineswepper");
         this.setSize(1295, 838);
